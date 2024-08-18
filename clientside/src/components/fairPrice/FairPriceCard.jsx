38bb310dc41de3c0e47d0057a906fd5802139818
Ayyanar <ayan.prof022@example.com>
2024-08-18 21:26:30 +0530

    m

--- clientside/src/components/fairPrice/FairPriceCard.jsx
+++ clientside/src/components/fairPrice/FairPriceCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const FairPriceCard = ({ data }) => {
     return (
-        <div className='fair-price-card p-4 border h-64  rounded shadow cursor-pointer hover:scale-105 transition-all' >
-            <img src={data.item_Image} alt={data.itemName} className='w-full h-40 object-cover mb-4 rounded ' />
-            <h2 className='text-lg font-semibold border-t-2 border-[#efe3e3]'>{data.itemName}</h2>
-            <p className='text-gray-600'>{data.category}</p>
+        <div className='fair-price-card p-4 border h-60 md:h-64  rounded shadow cursor-pointer hover:scale-105 transition-all' >
+            <img src={data.item_Image} alt={data.itemName} className='w-full h-36 md:h-40 object-cover mb-4 rounded ' />
+            <h2 className='md:text-md lg:text-lg text-slate-800 font-semibold border-t-2 border-[#efe3e3]'>{data.itemName}</h2>
+            <p className='text-gray-600 text-sm md:text-md pb-1'>{data.category}</p>
         </div>
     )
 }
--- clientside/src/pages/fairPrice/FairPrice.jsx
+++ clientside/src/pages/fairPrice/FairPrice.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const FairPrice = () => {
 
-  
+
 
   const { fairPriceCardData, isLoading } = useGetAllFairPrice()
   const [filterData, setFilterData] = useState([])
@@ -37,11 +37,12 @@ const FairPrice = () => {
 
 
   return (
-    <div className=' mx-auto w-full lg:px-9 py-1 sm:px-3 '>
-      <div className='flex items-center gap-4 justify-between overflow-x-auto scrollbar-hiden '>
+    <div className=' mx-auto w-full lg:px-14 py-2 md:py-3 px-2 '>
+
+      <div className='flex items-center gap-4 justify-between overflow-x-auto scrollbar-hiden  '>
         {filterData?.map((item) => (
           <div onClick={() => singleFilter(item.item_category)} key={item.id} className='cursor-pointer flex flex-col justify-center items-center  w-full md:w-1/3 p-2'>
-            <div className='h-12 w-12 md:h-16 md:w-16 rounded-full overflow-hidden p-2 bg-slate-200 flex items-center justify-center'>
+            <div className='h-12 w-12 md:h-16 lg:h-20 md:w-16 lg:w-20 rounded-full overflow-hidden p-2 bg-slate-200 flex items-center justify-center'>
               <img
                 src={item?.item_Image}
                 alt={item?.item_category}
@@ -53,9 +54,10 @@ const FairPrice = () => {
         ))}
       </div>
 
-      <div className='p-2  py-4'>
-        <div onClick={() => setFilterdCardData([])} className='mb-4 text-xl font-semibold cursor-pointer'>All Category</div>
-        <div className='grid lg:gap-8 md:gap-4 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2'>
+      <div className='  py-4 px-2 '>
+        <div onClick={() => setFilterdCardData([])} className='mb-4 text-xl font-bold cursor-pointer text-amber-500'>All Category</div>
+
+        <div className='grid gap-3 md:gap-5 lg:gap-9 grid-cols-2 md:grid-cols-4 lg:grid-cols-5'>
           {!isLoading && (filterdcardData.length > 0 ? filterdcardData : fairPriceCardData)?.map((item) => (
             <Link to={`/fairPrice-details/${item.itemName}/${item.category}`}>
               <FairPriceCard key={item.id} data={item} />
