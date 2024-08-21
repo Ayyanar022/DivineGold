de19ded333ef2de55529dbd5c0b36155db6f34b7
Ayyanar <ayan.prof022@example.com>
2024-08-21 16:47:55 +0530

    m

--- clientside/src/components/explore/JewllDesignCard.jsx
+++ clientside/src/components/explore/JewllDesignCard.jsx
@@ -19,16 +19,16 @@ const JewllDesignCard = ({ item }) => {
     return (
         <Link to={`/JewllDesign-details/${item?._id}`} className="  h-52   md:h-56 lg:h-60 shadow-md border m-2 rounded-lg overflow-hidden transform transition-transform hover:scale-105">
             <img src={item?.jewellImage[0]} alt="img" className="w-full h-32  md:h-36 lg:h-40  object-cover mb-4 rounded p-1 " />
-            <div className="px-2 lg:py-2 border-t">
-                <p className="text-md md:text-lg font-semibold text-gray-700 ">{item.jewellName}</p>
-                <div className='flex justify-between'>
+
+            <div className="px-2 py-1 bg-white flex">
+                <div className='w-3/4'>
+                    <p className="text-md md:text-lg font-semibold text-gray-700 whitespace-nowrap overflow-hidden text-ellipsis max-w-full ">{item.jewellName}</p>
                     <p className="text-sm text-gray-500 ">{item.jewellType}</p>
-                    <div className='flex gap-1.5 md:gap-3'>
-                        {/* <spam className="text-xs md:text-lg p-1 bg-green-300 hover:bg-green-500 transition-all rounded-full"><CiHeart /></spam> */}
-                        <button onClick={(event) => handleAaddtoCartfun(item?._id, event)} className=" text-xs md:text-lg  p-1 bg-orange-300 hover:bg-orange-500 transition-all rounded-full"><CiBag1 /></button>
 
-                    </div>
+                </div>
 
+                <div className='w-1/4 flex justify-center items-center '>
+                    <button onClick={(event) => handleAaddtoCartfun(item?._id, event)} className="bg-orange-400 hover:bg-orange-500 transition-all rounded-full p-2 "><CiBag1 /></button>
                 </div>
 
             </div>
--- clientside/src/pages/explore/Explore.jsx
+++ clientside/src/pages/explore/Explore.jsx
@@ -98,12 +98,11 @@ const Explore = () => {
   return <div className="w-full  flex mb-14 lg:mb-0">
     {/**filter  */}
 
-    {/* <aside className=" hidden md:block h-screen  overflow-y-auto"> */}
-    <aside className="hidden md:block  fixed top-0 left-0 w-52 h-full overflow-y-auto  bg-white border-r shadow-sm">
-      <nav className="h-full flex flex-col p-2  border-r shadow-sm">
+    <aside className="hidden md:block  fixed top-0 left-0 md:w-52 lg:w-56 h-full overflow-y-auto  bg-white border-r shadow-sm">
+      <nav className="h-full flex flex-col w-full   border-r shadow-sm">
 
-        <div className="p-4 pb-5 flex justify-between items-center ">
-          {!filterData ? <p className="text-lg font-semibold w-32 cursor-pointer">Filter</p> : <p onClick={handleFilterSubmit} className="text-xl w-32 cursor-pointer font-semibold">Clear</p>}
+        <div className="p-4 pb-3 mb-5 flex justify-between items-center bg-lime-300 pt-16 ">
+          {!filterData ? <p className="text-xl text-slate-800 font-medium tracking-wider uppercase  w-32 cursor-pointer">Filter</p> : <p onClick={handleFilterSubmit} className="text-xl font-medium uppercase text-slate-800 tracking-wider w-32 cursor-pointer">Clear</p>}
           <button className="p-1.5 bg-gray-50 hover:bg-gray-100 rounded-lg"><FaArrowRightArrowLeft /></button>
         </div>
 
@@ -111,16 +110,25 @@ const Explore = () => {
         <div className="flex-1 px-5">
           {/**Gender filter */}
           <div className="mb-5 ">
-            <div onClick={() => setGenderToggle(prev => !prev)} className="flex items-center gap-2">
-              <h3 className="text-md font-semibold pb-2">Gender</h3>
-              {genderToggle && <MdOutlineKeyboardArrowUp />}
-              {!genderToggle && <MdKeyboardArrowDown />}
+            <div onClick={() => setGenderToggle(prev => !prev)} className="flex justify-between items-center ">
+              <h3 className="text-md text-slate-700 uppercase tracking-wide font-semibold pb-2 ">Gender</h3>
+              <div>
+                {!genderToggle && <MdOutlineKeyboardArrowUp className="text-xl" />}
+                {genderToggle && <MdKeyboardArrowDown className="text-xl" />}
+              </div>
 
             </div>
 
             {genderToggle && (<div >
               {Gender && ConstantItemGender?.data?.map((item, index) => (
-                <div key={item?._id} className="flex items-center mb-1">
+                <div key={item?._id}
+
+                  // className="flex items-center py-1 px-3 gap-1 hover:bg-slate-300 " 
+                  className={`flex items-center py-1 px-3 gap-1 
+                  hover:bg-slate-300 
+                  ${selectedGendr.includes(item?.itemGender) ? 'bg-slate-200' : 'bg-white'}`}
+
+                >
                   <input
                     type="checkbox"
                     id={`gender-${item?._id}`}
@@ -130,7 +138,7 @@ const Explore = () => {
                     name={item?.itemGender}
                     checked={selectedGendr.includes(item?.itemGender)}
                   />
-                  <label htmlFor={`gender-${item?._id}`}>{item?.itemGender}</label>
+                  <label className="text-slate-700 text-md" htmlFor={`gender-${item?._id}`}>{item?.itemGender}</label>
                 </div>
               ))}
             </div>)}
@@ -139,14 +147,21 @@ const Explore = () => {
 
           {/**Types */}
           <div className="mb-5">
-            <div onClick={() => settypeToggle(prev => !prev)} className="flex items-center gap-2">
-              <h3 className="text-md font-semibold pb-2">Types</h3>
-              {typeToggle && <MdOutlineKeyboardArrowUp />}
-              {!typeToggle && <MdKeyboardArrowDown />}
+            <div onClick={() => settypeToggle(prev => !prev)} className="flex justify-between items-center gap-2">
+              <h3 className="text-md text-slate-700 uppercase tracking-wide font-semibold pb-2 ">Types</h3>
+              {!typeToggle && <MdOutlineKeyboardArrowUp className="text-xl" />}
+              {typeToggle && <MdKeyboardArrowDown className="text-xl" />}
             </div>
 
             {typeToggle && ConstantItemType?.data?.map((item, index) => (
-              <div key={item?._id} className="flex items-center mb-1">
+              <div key={item?._id}
+                // className="flex items-center py-1 px-3 gap-1 hover:bg-slate-300"
+
+                className={`flex items-center py-1 px-3 gap-1 
+                hover:bg-slate-300 
+                ${selectType.includes(item?.itemType) ? 'bg-slate-200' : 'bg-white'}`}
+
+              >
                 <input
                   type="checkbox"
                   id={`type${item?._id}`}
@@ -156,7 +171,7 @@ const Explore = () => {
                   name={item?.itemType}
                   checked={selectType.includes(item?.itemType)}
                 />
-                <label htmlFor={`type${item?._id}`} >{item?.itemType}</label>
+                <label className="text-slate-700 text-md" htmlFor={`type${item?._id}`} >{item?.itemType}</label>
               </div>
             ))}
 
@@ -164,14 +179,20 @@ const Explore = () => {
 
           {/**Category */}
           <div className="mb-16">
-            <div onClick={() => setCategoryToggle(prev => !prev)} className="flex items-center gap-2">
-              <h3 className="text-md font-semibold pb-2">Category</h3>
-              {categoryToggle && <MdOutlineKeyboardArrowUp />}
-              {!categoryToggle && <MdKeyboardArrowDown />}
+            <div onClick={() => setCategoryToggle(prev => !prev)} className="flex justify-between items-center gap-2">
+              <h3 className="text-md text-slate-700 uppercase tracking-wide font-semibold pb-2 ">Category</h3>
+              {!categoryToggle && <MdOutlineKeyboardArrowUp className="text-xl" />}
+              {categoryToggle && <MdKeyboardArrowDown className="text-xl" />}
             </div>
 
+
             {categoryToggle && ConstantItemCategory?.data?.map((item, index) => (
-              <div key={item?._id} className="flex items-center mb-1">
+              <div
+                key={item?._id}
+                className={`flex items-center py-1 px-3 gap-1 
+                hover:bg-slate-300 
+                ${selectCategory?.includes(item?.itemCategory) ? 'bg-slate-200' : 'bg-white'}`}
+              >
                 <input
                   type="checkbox"
                   id={`type${item?._id}`}
@@ -181,9 +202,10 @@ const Explore = () => {
                   name={item?.itemCategory}
                   checked={selectCategory?.includes(item?.itemCategory)}
                 />
-                <label htmlFor={`type${item?._id}`} >{item?.itemCategory}</label>
+                <label className="text-slate-700 text-md" htmlFor={`type${item?._id}`}>{item?.itemCategory}</label>
               </div>
             ))}
+
 
           </div>
 
@@ -289,8 +311,7 @@ const Explore = () => {
 
     {/**main */}
 
-    {/* <main className="p-2 w-full h-full "> */}
-    <main className="p-1 w-full ml-0 md:ml-52">
+    <main className="p-1 w-full ml-0 md:ml-52 pb-7">
       <div className="w-full">
         {nodilterData}
       </div >
