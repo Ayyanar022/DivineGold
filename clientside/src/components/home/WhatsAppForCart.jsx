c7652cf3bae9fc31f05c3ae9fd6d2e88a85e660f
Ayyanar <ayan.prof022@example.com>
2024-08-21 13:13:52 +0530

    m

--- clientside/src/components/home/WhatsAppForCart.jsx
+++ clientside/src/components/home/WhatsAppForCart.jsx
@@ -6,6 +6,7 @@ const WhatsAppForCart = ({ data }) => {
     const [userPhone, setUserPhone] = useState('');
     const [userMessage, setUserMessage] = useState('');
     const [userName, setUserName] = useState('');
+    const [village, setVillage] = useState('')
 
     const yourPhoneNumber = '919698358807';
 
@@ -25,7 +26,7 @@ const WhatsAppForCart = ({ data }) => {
 
         // Ensure that userMessage is properly encoded for WhatsApp
         const encodedMessage = encodeURIComponent(
-            `From: ${userPhone}\nUserName: ${userName}\nMessage:\n${userMessage}`
+            `From: ${userPhone}\nUserName: ${userName}\nVillage:${village}\nMessage:\n${userMessage}`
         ).replace(/%20/g, '+').replace(/\n/g, '%0A');
 
         // WhatsApp URL with encoded message
@@ -59,6 +60,17 @@ const WhatsAppForCart = ({ data }) => {
                             value={userName}
                             onChange={(e) => setUserName(e.target.value)}
                             placeholder="Enter your Name"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 font-medium mb-2">Your Vilage or City</label>
+                        <input
+                            type="text"
+                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                            value={village}
+                            onChange={(e) => setVillage(e.target.value)}
+                            placeholder="Enter your Village or City"
                             required
                         />
                     </div>
