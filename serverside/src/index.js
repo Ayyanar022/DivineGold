46aa740872c64a93341df56a60badd981183863c
Ayyanar <ayan.prof022@example.com>
2024-08-25 16:08:51 +0530

    m

--- serverside/src/index.js
+++ serverside/src/index.js
@@ -4,24 +4,23 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute.js";
 import adminRoutes from "./routes/AdminRoute.js";
-import fairPriceRoutes from "./routes/FairPriceRout.js"
-import exploreRoutes from "./routes/ExoloreRoute.js"
+import fairPriceRoutes from "./routes/FairPriceRout.js";
+import exploreRoutes from "./routes/ExoloreRoute.js";
 import cartRoute from "./routes/CartRout.js";
-import customerSale from "./routes/CustomerSaleRoute.js"
-import salefilter from "./routes/SaleFilterRoute.js"
+import customerSale from "./routes/CustomerSaleRoute.js";
+import salefilter from "./routes/SaleFilterRoute.js";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 app.use("/api/my/user",myUserRoute);
 app.use("/api/admin",adminRoutes);
 app.use("/api/fairPrice",fairPriceRoutes);
-app.use("/api/explore",exploreRoutes)
-app.use("/api/cart",cartRoute)
-app.use("/api/admin/customersale",customerSale)
-app.use("/api/admin/filter",salefilter)
+app.use("/api/explore",exploreRoutes);
+app.use("/api/cart",cartRoute);
+app.use("/api/admin/customersale",customerSale);
+app.use("/api/admin/filter",salefilter);
 
 mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("connected to DB"))
 
@@ -30,3 +29,4 @@ const PORT = process.env.PORT || 7000;  // Use the port provided by Render or fa
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
