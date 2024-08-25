2b223d064946ff1fa08412ef0e6deac63d9f39e7
Ayyanar <ayan.prof022@example.com>
2024-08-25 18:35:05 +0530

    m

--- clientside/src/auth/Auth0ProviderWithNavigate.js
+++ clientside/src/auth/Auth0ProviderWithNavigate.js
@@ -8,7 +8,8 @@ const Auth0ProviderWithNavigate = ({ children }) => {
 
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  // const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const redirectUri = `http://localhost:3000`
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
 
