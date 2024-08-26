d0b16c7f9673e88215a4ed0796b16270005f5d71
Ayyanar <ayan.prof022@example.com>
2024-08-26 13:32:29 +0530

    m

--- clientside/src/components/WhatsAppForm.jsx
+++ clientside/src/components/WhatsAppForm.jsx
@@ -21,50 +21,48 @@ const WhatsAppForm = () => {
     };
 
     return (
-        <div className="flex justify-center items-center h-[calc(100vh-108px)] bg-late-100">
-            <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
-                <h2 className="text-xl md:text-2xl font-bold text-center text-green-600 mb-6">Send a WhatsApp Message</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-2">Your Phone Number</label>
-                        <input
-                            type="text"
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-                            value={userPhone}
-                            onChange={(e) => setUserPhone(e.target.value)}
-                            placeholder="Enter your phone number"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-2">Your Name</label>
-                        <input
-                            type="text"
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                            placeholder="Enter your Name"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-medium mb-2">Your Message</label>
-                        <textarea
-                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-                            value={userMessage}
-                            onChange={(e) => setUserMessage(e.target.value)}
-                            placeholder="Type your message"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition duration-300"
-                    >
-                        Send WhatsApp Message
-                    </button>
-                </form>
-            </div>
+        <div className=" mx-auto w-full max-w-md rounded-lg md:shadow-md p-3 md:p-6">
+            <h2 className="text-[17px] md:text-xl font-bold text-center text-green-600 mb-4 md:mb-6">Send a WhatsApp Message</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3 md:mb-4">
+                    <label className=" text-[14px] md:text-md block text-gray-700 font-medium mb-1 md:mb-2">Your Phone Number</label>
+                    <input
+                        type="text"
+                        className="w-full px-3 py-1 md:py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-green-500 placeholder:text-sm"
+                        value={userPhone}
+                        onChange={(e) => setUserPhone(e.target.value)}
+                        placeholder="Enter your phone number"
+                        required
+                    />
+                </div>
+                <div className="mb-3 md:mb-4">
+                    <label className="text-[14px] md:text-md block text-gray-700 font-medium mb-1 md:mb-2">Your Name</label>
+                    <input
+                        type="text"
+                        className="w-full px-3 py-1 md:py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-green-500 placeholder:text-sm"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                        placeholder="Enter your Name"
+                        required
+                    />
+                </div>
+                <div className="mb-3 md:mb-4">
+                    <label className="text-[14px] md:text-md block text-gray-700 font-medium mb-1 md:mb-2">Your Message</label>
+                    <textarea
+                        className="w-full px-3 py-1 md:py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-green-500 placeholder:text-sm"
+                        value={userMessage}
+                        onChange={(e) => setUserMessage(e.target.value)}
+                        placeholder="Type your message"
+                        required
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition duration-300"
+                >
+                    Send WhatsApp Message
+                </button>
+            </form>
         </div>
     );
 };
--- clientside/src/pages/contectInfo/ContectInfo.jsx
+++ clientside/src/pages/contectInfo/ContectInfo.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 import WhatsAppForm from '../../components/WhatsAppForm'
 
 const ContectInfo = () => {
-  return (
-    <div className='px-4 mx-auto transition-all duration-150'>
-      <WhatsAppForm />
+  return (  
+    <div className='p-4 mx-auto transition-all duration-150 pb-[68px] pd:pb-[76px]   flex flex-col gap-3 md:gap-6  '>
+      <div className="flex flex-col items-center justify-center gap-4 p-4  bg-amber-50  rounded-md">
+        <h2 className="text-[15px] sm:text-[17px] sm:text-xl md:text-2xl text-gray-800 font-bold text-center">
+          Have a Question? We're Here to Help!
+        </h2>
+        <p className="text-[13px] md:text-md lg:text-[17px]  text-gray-600 text-center">
+          If you have any queries or need assistance, feel free to reach out to us. Our team is always ready to provide the support you need.
+        </p>
+      </div>
+
+      <div className=''>
+        <WhatsAppForm />
+      </div>
+
     </div>
   )
 }
