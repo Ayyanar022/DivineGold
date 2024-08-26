e7a01353293eb2f5fb4d91206227c8d7d24dbe9c
Ayyanar <ayan.prof022@example.com>
2024-08-26 12:44:05 +0530

    m

--- clientside/src/components/forms/UserProfileForm.jsx
+++ clientside/src/components/forms/UserProfileForm.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useForm } from "react-hook-form";
 import { z } from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -30,82 +30,117 @@ const UserProfileForm = ({ currentUser, onSave, isLoading, refetchUserData }) =>
         },
     });
 
+
+    // UPDATE CUSTOMER INFO
     const onSubmit = async (data) => {
         if (onSave) {
             const response = await onSave(data);
 
-            if (!response.success) {
-                toast.warning(response.message.toString())
-            }
-            else if (response.success) {
-                toast.success(response.message.toString())
-                await refetchUserData();
-                reset(await refetchUserData());
+            if (response.success) {
+                toast.success("Updated sucessfully .. please Refresh ");
+
+                // Re-fetch the latest user data
+                const { data: updatedUser } = await refetchUserData();
+
+                // Reset the form with the updated user data
+                if (updatedUser) {
+                    reset({
+                        email: updatedUser?.email || '',
+                        name: updatedUser?.name || '',
+                        mobileNo: updatedUser?.mobileNo?.toString() || '', // Convert to string
+                        address: updatedUser?.address || '',
+                        village: updatedUser?.village || '',
+                        city: updatedUser?.city || '',
+                        bonousCode: updatedUser?.bonousCode || '',
+                    });
+                }
+
+            } else {
+                toast.warning(response.message.toString());
             }
         } else {
-            toast.info("Please try again..")
+            toast.info("Please try again..");
         }
     };
 
+
+
     return (
 
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 md:p-4  bg-white w-full justify-center ">
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 lg:p-4  bg-white w-full justify-center ">
 
-            <div className="w-full md:w-1/2 lg:w-2/5 bg-white p-6 rounded-lg shadow-md border">
-                <h2 className="text-xl font-bold mb-4 text-slate-700 uppercase">User Profile Form</h2>
+            <div className="w-full md:w-1/2 lg:w-2/5 bg-white p-1 md:p-5 lg:p-6 md:rounded-lg md:shadow-md md:border">
+                <h2 className="text-[17px] md:text-xl font-bold  text-cyan-900 uppercase ">Update Your Details </h2>
+                <p className='w-fit text-[11px] md:text-[12px] lg:text-[13px]   text-amber-800 mb-2 md:mb-5'>Use reference TRESURE CODE and get extra prizeToken </p>
                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Email</label>
-                        <input {...register('email')} disabled className="mt-1 block w-full text-gray-500 p-2 border border-gray-300 rounded-md" />
+                        <input {...register('email')} disabled
+                            className="text-sm md:text-md mt-1 block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md outline-none"
+                        />
                         {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Full Name</label>
-                        <input {...register('name')} className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
+                        <input {...register('name')}
+                            className="text-sm md:text-md mt-1  block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md  outline-1 focus:shadow"
+                        />
                         {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>}
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Mobile No</label>
-                        <input {...register('mobileNo')} className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
+                        <input {...register('mobileNo')}
+                            className="text-sm md:text-md mt-1 block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md  outline-1 focus:shadow"
+                        />
                         {errors.mobileNo && <p className="text-red-500 text-sm mt-1">{errors.mobileNo.message}</p>}
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Address</label>
-                        <input {...register('address')} className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
+                        <input {...register('address')}
+                            className="text-sm md:text-md mt-1 block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md  outline-1 focus:shadow"
+                        />
                         {errors.address && <p className="text-red-500 text-sm mt-1">{errors.address.message}</p>}
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">Village</label>
-                        <input {...register('village')} className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
+                        <input {...register('village')}
+                            className="text-sm md:text-md mt-1 block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md  outline-1 focus:shadow"
+                        />
                         {errors.village && <p className="text-red-500 text-sm mt-1">{errors.village.message}</p>}
                     </div>
                     <div>
                         <label className="block text-sm font-medium text-gray-700">City</label>
-                        <input {...register('city')} className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
+                        <input {...register('city')}
+                            className="text-sm md:text-md mt-1 block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md  outline-1 focus:shadow"
+                        />
                         {errors.city && <p className="text-red-500 text-sm mt-1">{errors.city.message}</p>}
                     </div>
+
                     {!(currentUser.name || currentUser.mobileNo || currentUser.address || currentUser.village || currentUser.city) && <div>
                         <label className="block text-sm font-medium text-gray-700">Super Code</label>
-                        <input {...register('bonousCode')} className="mt-1 block w-full p-2 border border-gray-300 rounded-md" />
+                        <input {...register('bonousCode')}
+                            className=" text-sm md:text-md mt-1 block w-full text-gray-500 p-1.5 md:p-2 border border-gray-300 rounded-md  outline-1 focus:shadow"
+                        />
                         {errors.bonousCode && <p className="text-red-500 text-sm mt-1">{errors.bonousCode.message}</p>}
                     </div>}
 
-                    <button type="submit" disabled={isLoading} className="w-full bg-pink-600 uppercase font-semibold text-white py-2 rounded-md hover:bg-pink-700 transition-all duration-200">
-                        {isLoading ? 'Loading...' : 'Update Your Account'}
+                    <button type="submit" disabled={isLoading} className="w-full text-sm md:text-md bg-cyan-700 uppercase tracking-wider shadow font-semibold text-white py-1.5 lg:py-2 rounded-md hover:bg-cyan-800 hover:shadow-md transition-all duration-200">
+                        {isLoading ? 'Loading...' : 'Update'}
                     </button>
                 </form>
             </div>
 
 
-            <div className="w-full md:w-1/2 lg:w-2/5 bg-white p-6 rounded-lg shadow-md border">
-                <h2 className="text-xl font-bold uppercase mb-4 text-slate-700">User Details</h2>
+            <div className="w-full md:w-1/2 lg:w-2/5 bg-white p-1 md:p-5 lg:p-6 rounded-lg md:shadow-md md:border">
+                <h2 className="text-[17px] md:text-xl font-bold  text-cyan-900 uppercase">Refer & Get</h2>
+                <p className='w-fit text-[11px] md:text-[12px] lg:text-[13px]   text-amber-800 mb-2 md:mb-5'>Share your treasure code, and both will get extra prize tokens. </p>
 
-                <div className=' flex flex-col lg:flex-row gap-2 lg:items-center'>
-                    <p className='text-lg font-semibold  text-slate-700'>Treasure Code : </p>
-                    <p className='text-slate-700 text-sm'>{currentUser.bonousCode}</p>
+                <div className=' flex flex-col lg:flex-row md:gap-2 lg:items-center'>
+                    <p className=' text-md md:text-lg font-semibold   text-slate-700'>Treasure Code : </p>
+                    <p className='text-slate-700 text-[13px] md:text-md'>{currentUser.bonousCode}</p>
                 </div>
                 <div className='flex gap-3 md:gap-5 mt-3 items-center '>
-                    <p className='text-lg font-semibold text-slate-700'>Prize Token : </p>
+                    <p className=' text-md md:text-lg font-semibold text-slate-700'>Prize Token : </p>
                     <p className='text-slate-800 font-semibold text-sm md:text-md'>{currentUser.bonousePoints}</p>
                 </div>
             </div>
